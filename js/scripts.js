009dcf9678236ca5a312ba247fcaf360d99ee16c
bkenol <bjk396@nyu.edu>
2017-10-30 03:09:58 -0400

    90% Done
    Almost done with the website, just needs some fine tuning

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var music = document.getElementById('music');
+
+function playAudio() {
+	if (music.paused) {
+		music.play();
+		pButton.className = "";
+		pButton.className = "pause";
+	} else {
+		music.pause();
+		pButton.className = "";
+		pButton.className = "play";
+	}
+}
